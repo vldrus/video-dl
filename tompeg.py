bda808f2d8c245ae0a7903b339afafbe3267844a
Vladislav Alekseev <65050323+vldrus@users.noreply.github.com>
2022-07-26 00:35:26 +0300

    Add files via upload

---
+++ tompeg.py
@@ -0,0 +1,43 @@
+#!/bin/env python3
+
+
+import sys
+import pathlib
+import subprocess
+
+
+if len(sys.argv) < 2:
+    print(f'Usage: {sys.argv[0]} input_file.webm')
+    sys.exit(1)
+
+
+DEFAULT_OUTPUT_WIDTH = '720'
+DEFAULT_OUTPUT_CRF = '24'
+DEFAULT_OUTPUT_AUDIO = '96k'
+
+
+input_file = sys.argv[1]
+output_file = pathlib.Path(input_file).stem + '_out.mp4'
+
+print('Input file: ', input_file)
+print('Output file:', output_file)
+
+
+output_width = input(f'Enter width [{DEFAULT_OUTPUT_WIDTH}]: ') or DEFAULT_OUTPUT_WIDTH
+output_crf = input(f'Enter crf [{DEFAULT_OUTPUT_CRF}]: ') or DEFAULT_OUTPUT_CRF
+output_audio = input(f'Enter audio [{DEFAULT_OUTPUT_AUDIO}]: ') or DEFAULT_OUTPUT_AUDIO
+
+
+output_scale=f'-2:{output_width}:flags=lanczos'
+output_comment=f'WIDTH={output_width},CRF={output_crf},AUDIO={output_audio},INPUT={input_file}'
+
+
+ffmpeg_command=f'''ffmpeg -i "{input_file}" \
+                          -vf scale="{output_scale}" \
+                          -crf "{output_crf}" \
+                          -b:a "{output_audio}" \
+                          -map_metadata -1 \
+                          -metadata comment="{output_comment}" \
+                          {output_file}'''
+
+subprocess.call(ffmpeg_command, shell=True)
