b6fe487568db0798f99b6f22d5fed5bef0253f1d
Vladislav Alekseev <65050323+vldrus@users.noreply.github.com>
2022-07-28 01:27:02 +0300

    Update tompeg.py

--- tompeg.py
+++ tompeg.py
@@ -16,23 +16,19 @@ if len(sys.argv) < 2:
     print(f'Usage: {sys.argv[0]} input_file.webm')
     sys.exit(1)
 
-
 input_file = sys.argv[1]
 output_file = pathlib.Path(input_file).parent / f'{pathlib.Path(input_file).stem}_out.mp4'
 
 print('Input file: ', input_file)
 print('Output file:', output_file)
 
-
 output_width = input(f'Enter width [{DEFAULT_OUTPUT_WIDTH}]: ') or DEFAULT_OUTPUT_WIDTH
 output_crf = input(f'Enter crf [{DEFAULT_OUTPUT_CRF}]: ') or DEFAULT_OUTPUT_CRF
 output_audio = input(f'Enter audio [{DEFAULT_OUTPUT_AUDIO}]: ') or DEFAULT_OUTPUT_AUDIO
 
-
 output_scale=f'-2:{output_width}:flags=lanczos'
 output_preset=f'{DEFAULT_OUTPUT_PRESET}'
 output_comment=f'WIDTH={output_width},CRF={output_crf},AUDIO={output_audio},INPUT={pathlib.Path(input_file).name}'
-
 
 ffmpeg_command=f'''ffmpeg -i "{input_file}" \
                           -vf scale="{output_scale}" \
