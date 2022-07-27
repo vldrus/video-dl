af74fd342cbd07faeb370060f143e571a8856ab0
Vladislav Alekseev <65050323+vldrus@users.noreply.github.com>
2022-07-28 01:27:58 +0300

    Update tompeg.py

--- tompeg.py
+++ tompeg.py
@@ -25,7 +25,6 @@ print('Output file:', output_file)
 output_width = input(f'Enter width [{DEFAULT_OUTPUT_WIDTH}]: ') or DEFAULT_OUTPUT_WIDTH
 output_crf = input(f'Enter crf [{DEFAULT_OUTPUT_CRF}]: ') or DEFAULT_OUTPUT_CRF
 output_audio = input(f'Enter audio [{DEFAULT_OUTPUT_AUDIO}]: ') or DEFAULT_OUTPUT_AUDIO
-
 output_scale=f'-2:{output_width}:flags=lanczos'
 output_preset=f'{DEFAULT_OUTPUT_PRESET}'
 output_comment=f'WIDTH={output_width},CRF={output_crf},AUDIO={output_audio},INPUT={pathlib.Path(input_file).name}'
