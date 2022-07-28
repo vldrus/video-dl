b14819aacb22feb1f246f97925e9aac3f6fb656c
Vladislav Alekseev <65050323+vldrus@users.noreply.github.com>
2022-07-28 14:19:10 +0300

    Update tompeg.py

--- tompeg.py
+++ tompeg.py
@@ -8,6 +8,7 @@ import subprocess
 
 DEFAULT_OUTPUT_WIDTH  = '720'
 DEFAULT_OUTPUT_CRF    = '24'
+DEFAULT_OUTPUT_VIDEO  = '900k'
 DEFAULT_OUTPUT_AUDIO  = '96k'
 DEFAULT_OUTPUT_PRESET = 'veryslow'
 
@@ -23,19 +24,32 @@ print('Input file: ', input_file)
 print('Output file:', output_file)
 
 output_width   = input(f'Enter width [{DEFAULT_OUTPUT_WIDTH}]: ') or DEFAULT_OUTPUT_WIDTH
-output_crf     = input(f'Enter crf [{DEFAULT_OUTPUT_CRF}]: ')     or DEFAULT_OUTPUT_CRF
+
+output_crf     = input(f'Enter crf [{DEFAULT_OUTPUT_CRF}]: ') or DEFAULT_OUTPUT_CRF
+output_quality = f'-crf "{output_crf}"'
+
+if output_crf.lower() == 'no':
+    output_video = input(f'Enter video [{DEFAULT_OUTPUT_VIDEO}]: ') or DEFAULT_OUTPUT_VIDEO
+    output_quality = f'-b:v "{output_video}"'
+
 output_audio   = input(f'Enter audio [{DEFAULT_OUTPUT_AUDIO}]: ') or DEFAULT_OUTPUT_AUDIO
 output_scale   = f'-2:{output_width}:flags=lanczos'
 output_preset  = f'{DEFAULT_OUTPUT_PRESET}'
-output_comment = f'WIDTH={output_width},CRF={output_crf},AUDIO={output_audio},INPUT={pathlib.Path(input_file).name}'
+output_comment = f'CRF={output_crf},PRESET={output_preset},INPUT={pathlib.Path(input_file).name}'
 
 ffmpeg_command=f'''ffmpeg -i "{input_file}" \
                           -vf scale="{output_scale}" \
                           -preset "{output_preset}" \
-                          -crf "{output_crf}" \
+                          {output_quality} \
                           -b:a "{output_audio}" \
                           -map_metadata "-1" \
                           -metadata comment="{output_comment}" \
                           "{output_file}" '''
+
+print()
+print('Executing ffmpeg with the following parameters:')
+print()
+print(ffmpeg_command)
+print(flush=True)
 
 subprocess.call(ffmpeg_command, shell=True)
