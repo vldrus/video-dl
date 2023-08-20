9bcdf2e0ec2f59f12e3f63fb4cdb553032a9f462
Vlad <65050323+vldrus@users.noreply.github.com>
2023-08-20 19:55:11 +0300

    video-dl

--- video-dl.py
+++ video-dl.py
@@ -16,11 +16,14 @@ else:
 
 print()
 
+need_pause = False
+
 if len(sys.argv) > 1:
     video_link = sys.argv[1]
     print(f'Video link: {video_link}')
 else:
     video_link = input('Enter video link: ')
+    need_pause = True
 
 print()
 
@@ -38,3 +41,6 @@ video_merge  = 'mkv'
 print()
 
 subprocess.call(f'"{exe_file}" --no-playlist --format "{video_format}" --output "{video_output}" --merge-output-format "{video_merge}" "{video_link}"', shell=True)
+
+if need_pause:
+    input('Press <Enter> to close...')
